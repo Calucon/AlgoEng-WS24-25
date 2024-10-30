7b3396b91ec32fd87aba9d69ef0e58495cff0e7a
Simon Schwitz <simon@calucon.de>
2024-10-30 12:51:39 +0100

    create barebone strcuture for EM Merge Sort

--- src/EM_MergeSort/main.cpp
+++ src/EM_MergeSort/main.cpp
@@ -9,10 +9,8 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    char *strEnd; // just used for long parsing
     auto inFilePath = argv[1];
     auto outFilePath = argv[2];
-    // auto intCount = strtol(argv[2], &strEnd, 10);
 
     cout << "IN: " << inFilePath << " | OUT: " << outFilePath << endl;
 
