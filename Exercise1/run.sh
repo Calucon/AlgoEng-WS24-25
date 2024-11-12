0af34154e2a265d0f8540942563cfa1f5e4c41a7
Simon Schwitz <simon@calucon.de>
2024-11-12 11:05:23 +0100

    added run script for exercise 1

--- Exercise1/run.sh
+++ Exercise1/run.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
+#############################################
+#              BUILD & PREPARE              #
+#############################################
+
 pushd ..
 
 /bin/bash build.sh
-/bin/bash gendata.sh
+echo -e "\n\n"
+
+read -p "Do you want to (re-)generate input data files? [y/N]: " -n 1 -r
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    /bin/bash gendata.sh
+    echo -e "\n\n"
+fi
+
+#############################################
+#                  CONFIG                   #
+# Uncomment options if you like to run them #
+#############################################
+
+#INPUT_FILE="data/10.dat"
+#INPUT_FILE="data/10k.dat"
+INPUT_FILE="data/100k.dat"
+#INPUT_FILE="data/1M.dat"  # 4MB file
+#INPUT_FILE="data/10M.dat" # 40MB file
+#INPUT_FILE="data/1B.dat"  # 4GB file
+
+BLOCK_SIZE=10000 # large B
+
+PRINT_PROGRESS="silent"
+#PRINT_PROGRESS="progress" # uncomment to print progress to console
+
+#############################################
+#                  EXECUTE                  #
+#############################################
+
+# run merge sort
+./bin/EM_MergeSort $INPUT_FILE "data/out.dat" $BLOCK_SIZE $PRINT_PROGRESS
+# analyze file
+./bin/FileAnalyzer "data/out.dat" silent
