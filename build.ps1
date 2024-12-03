230fb9079e296badad410f5d578400271d9b879d
Simon Schwitz <simon@calucon.de>
2024-12-03 11:12:58 +0100

    improve build script + include binaries

--- build.ps1
+++ build.ps1
@@ -1,28 +1,26 @@
-If(!(test-path -PathType container "bin"))
-{
+If (!(test-path -PathType container "bin")) {
       New-Item -ItemType Directory -Path "bin"
 }
 
-If(!(test-path -PathType container "build"))
-{
+If (!(test-path -PathType container "build")) {
       New-Item -ItemType Directory -Path "build"
 }
 
 $GCC_PATH = (Get-Command gcc).Path
 $GPP_PATH = (Get-Command g++).Path
 
-if([string]::IsNullOrWhitespace($GCC_PATH)){
+if ([string]::IsNullOrWhitespace($GCC_PATH)) {
       Write-Error -Message "GCC Compiler not found. Please add it to your path!" -Category NotInstalled
       exit
 }
 
-if([string]::IsNullOrWhitespace($GPP_PATH)){
+if ([string]::IsNullOrWhitespace($GPP_PATH)) {
       Write-Error -Message "G++ Compiler not found. Please add it to your path!" -Category NotInstalled
       exit
 }
 
-$SC=(Join-Path $PSScriptRoot ".").Split(":").Get(1)
-$BC=(Join-Path $PSScriptRoot "build").Split(":").Get(1)
+$SC = (Join-Path $PSScriptRoot ".")
+$BC = (Join-Path $PSScriptRoot "build")
 
-cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=$GCC_PATH -DCMAKE_CXX_COMPILER:FILEPATH=$GPP_PATH -SC:$SC -Bc:$BC -G "MinGW Makefiles"
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=$GCC_PATH -DCMAKE_CXX_COMPILER:FILEPATH=$GPP_PATH -S$SC -B$BC -G "MinGW Makefiles"
 cmake --build build/ --config Release --target all --
--- build.sh
+++ build.sh
@@ -3,5 +3,20 @@
 mkdir -p bin/
 mkdir -p build/
 
-cmake -B build/ -D CMAKE_BUILD_TYPE=Release .
-cmake --build build/
+GCC=$(command -v gcc)
+if [ -z "${GCC}" ]; then
+    echo "GCC missing! Please add it to your path"
+    exit
+fi
+
+GPP=$(command -v g++)
+if [ -z "${GPP}" ]; then
+    echo "G++ missing! Please add it to your path"
+    exit
+fi
+
+SC=$PWD
+BC="$SC/build/"
+
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=$GCC -DCMAKE_CXX_COMPILER:FILEPATH=$GPP -S$SC -B$BC -G "Unix Makefiles"
+cmake --build build/ --config Release --target all --
